eaec8504fe7b5f173c72b03a1901397b8fc55998
Phanwasan <plugindiva@gmail.com>
2026-09-22 10:14:58 +0700

    Connect client to Render server

--- config.js
+++ config.js
@@ -3,9 +3,4 @@
 // Server Configuration
 // ========================================
 
-// ตอนทดสอบในเครื่อง
-const SERVER_URL = "http://localhost:3000";
-
-// เมื่อนำ Server ขึ้น Render แล้ว
-// เปลี่ยนด้านบนเป็นประมาณนี้:
-// const SERVER_URL = "https://your-server-name.onrender.com";
+const SERVER_URL = "https://dmitslap-war.onrender.com";
